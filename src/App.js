c8de6c61bf2f5d487c0e3dbfa561506a15187e67
Franktheknight <frankli1871@gmail.com>
2020-11-21 22:51:09 -0800

    Adding forms in forms folder

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import ForYou from './components/mainpage/foryou';
-import Trending from './components/mainpage/trendingposts'
-import Home from './components/coverpage/home'
+import Trending from './components/mainpage/trendingposts';
+import Home from './components/coverpage/home';
+//below are the forms, I am not sure if you need them in App.js
+//import Login from './components/forms/login';
+//import Register from './components/forms/register';
+//import NewPost from './components/forms/newpost';
 
-
-let login = false;
+let userlogin = true;
 
 function App() {
-  if (login) {
+  if (userlogin) {
     return (
       <div className="App">
         <SearchBar />
         <Trending />
         <ForYou />
       </div>
+      
     );
   }
   else {
