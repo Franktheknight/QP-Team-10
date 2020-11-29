d6a4c7c6a4c37bd472198062df60e6a333030d47
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2020-11-29 12:53:05 -0800

    Added preprocessing
    Imported keras.preprocessing

--- distiller/diaries/api.py
+++ distiller/diaries/api.py
@@ -2,6 +2,9 @@ from diaries.models import Diary
 from rest_framework import viewsets, permissions
 from .serializers import DiarySerializer
 
+#Added by Kanishk, 11/29/20
+from keras import preprocessing
+
 # Lead Viewset
 
 
@@ -25,3 +28,15 @@ class DiaryViewPopularSet(viewsets.ModelViewSet):
   def update(self, request, *args, **kwargs):
     kwargs['partial'] = True
     return super.update(request, *args, **kwargs)
+
+#Added by Kanishk, 11/29/20
+class DiaryTensorflowPreprocessing(viewsets.ModelViewSet):
+  
+  queryset = Diary.objects.all()
+  intermediate = [item.entry for item in queryset]
+  
+  def preprocessing(input_string):
+    tokenizer = preprocessing.text.tokenizer_from_json('tokenizer.json')
+    tokenized_inputs = tokenizer.texts_to_sequences(input_string)
+    padded_inputs = preprocessing.sequence.pad_sequences(tokenized_inputs, maxlen=1000)
+    return padded_inputs #This will be sent in the POST request to serving
